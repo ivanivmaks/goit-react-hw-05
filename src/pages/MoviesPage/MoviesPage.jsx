d56f6ad897499eb67227416bc4441eba610fc1ef
ivaniv_maks <ivanivmaks0@gmail.com>
2024-05-25 12:44:31 +0300

    updated

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { fetchSearch } from "../../API";
 import SearchMovieForm from "../../components/SearchMovieForm/SearchMovieForm";
 import Loader from "../../components/Loader/Loader";
 import MovieList from "../../components/MovieList/MovieList";
-// import css from "./MoviesPage.module.css";
 
 export default function MoviesPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [searchFilms, setSearchFilms] = useState([]);
   const [loading, setLoading] = useState(false);
   const [noMovies, setNoMovies] = useState(false);
+
+  const movieQuery = searchParams.get("query") || "";
+
+  useEffect(() => {
+    if (movieQuery) {
+      searchMovies(movieQuery);
+    }
+  }, [movieQuery]);
 
   const searchMovies = (movie) => {
     setLoading(true);
@@ -22,14 +31,18 @@ export default function MoviesPage() {
       .finally(() => setLoading(false));
   };
 
+  const handleSearch = (movie) => {
+    setSearchParams({ query: movie });
+  };
+
   return (
     <div>
-      <SearchMovieForm onSearch={searchMovies} />
-      {searchFilms && <MovieList films={searchFilms} />}
+      <SearchMovieForm onSearch={handleSearch} />
+      {loading && <Loader />}
       {noMovies && (
         <p>There is no movies with this request. Please, try again</p>
       )}
-      {loading && <Loader />}
+      {searchFilms && <MovieList films={searchFilms} />}
     </div>
   );
 }
